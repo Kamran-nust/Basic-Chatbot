b1e0c2a24287170edf8fc56fd62aa151379654f7
Muhammad Kamran <kamran_nust@yahoo.com>
2025-10-29 10:00:16 -0400

    Added comments and explanation to the code

--- chatbot.py
+++ chatbot.py
@@ -1,9 +1,20 @@
+
 from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 model_name = "facebook/blenderbot-400M-distill"
 # Load model (download on first run and reference local installation for consequent runs)
 model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 
+# There are several things you'll do to have an effective conversation with your chatbot.
+# Before interacting with your model, you need to initialize an object where you can store your conversation history.
+#     Initialize object to store conversation history
+# Afterward, you'll do the following for each interaction with the model:
+    #     Encode conversation history as a string
+    #     Fetch prompt from user
+    #     Tokenize (optimize) prompt
+    #     Generate output from the model using prompt and history
+    #     Decode output
+    #     Update conversation history
 conversation_history=[]
 while True:
     history_string= "\n".join(conversation_history)
